72045f407d35b1f5e05c7da391d368728759488a
Sunbir Gill <sunbir.gill@gmail.com>
2018-01-15 09:45:59 -0800

    refactoring

--- StickerUtil.swift
+++ StickerUtil.swift
@@ -9,16 +9,16 @@ import UIKit
 
 extension CGRect {
 
-    static func aspectFit(_ size: CGSize, intoRect: CGRect) -> CGRect {
-        var rect = CGRect.zero
-        let aspectWidth = intoRect.width / size.width
-        let aspectHeight = intoRect.height / size.height
+    func aspectFit(_ size: CGSize) -> CGRect {
+        var result = CGRect.zero
+        let aspectWidth = self.width / size.width
+        let aspectHeight = self.height / size.height
         let aspectRatio = min(aspectWidth, aspectHeight)
-        rect.size.width = size.width * aspectRatio;
-        rect.size.height = size.height * aspectRatio;
-        rect.origin.x = (intoRect.width - rect.size.width) / 2.0;
-        rect.origin.y = (intoRect.height - rect.size.height) / 2.0;
-        return rect
+        result.size.width = size.width * aspectRatio;
+        result.size.height = size.height * aspectRatio;
+        result.origin.x = (self.width - result.size.width) / 2.0;
+        result.origin.y = (self.height - result.size.height) / 2.0;
+        return result
     }
 
 }
@@ -27,9 +27,10 @@ extension FileManager {
 
     static func getTempFilePath(_ fileExtension: String) -> String? {
         let fileName = String(format: "%@.%@", ProcessInfo.processInfo.globallyUniqueString, fileExtension)
-        guard let fileUrl = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(fileName) else {
-            // Failed to construct temporary file path.
-            return nil
+        guard let fileUrl = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(fileName)
+            else {
+                // Failed to construct temporary file path.
+                return nil
         }
         return fileUrl.path
     }
@@ -38,23 +39,22 @@ extension FileManager {
 
 extension UIImage {
 
-    // See: https://developer.apple.com/documentation/messages
-    // Ordered from largest to smallest.
-    private static let OPTIMAL_STICKER_DIMENSIONS_PX: [CGFloat] = [ 618.0, 408.0, 300.0 ]
-    private static let MAX_STICKER_FILE_SIZE_BYTES: Int32 = 500 * 1024
-
-    public func hasAlpha() -> Bool {
+    func hasAlpha() -> Bool {
         guard let cgImage = self.cgImage else {
-            // Fail open. I.e. if we can't determine whether the image has an alpha channel we assume it does.
+            // Fail open. I.e. if we can't determine whether the image has an alpha channel
+            // we assume it does.
             return true
         }
         let alpha = cgImage.alphaInfo
-        return alpha == .first || alpha == .last || alpha == .alphaOnly || alpha == .premultipliedFirst || alpha == .premultipliedLast
+        return alpha == .first
+            || alpha == .last
+            || alpha == .alphaOnly
+            || alpha == .premultipliedFirst
+            || alpha == .premultipliedLast
     }
 
-    public func resizeImageToFit(_ size: CGSize) -> UIImage? {
-        let targetRect = CGRect(origin: CGPoint.zero, size: size)
-        let imageRect = CGRect.aspectFit(self.size, intoRect: targetRect)
+    func resizeImageToFit(_ size: CGSize) -> UIImage? {
+        let imageRect = CGRect(origin: CGPoint.zero, size: size).aspectFit(self.size)
         let imageSize = imageRect.size
         UIGraphicsBeginImageContextWithOptions(imageSize, hasAlpha(), 1.0)
         self.draw(in: CGRect(origin: CGPoint.zero, size: imageSize))
@@ -67,10 +67,17 @@ extension UIImage {
         return image
     }
 
-    public func createSticker(_ localizedDescription: String) throws -> MSSticker? {
+    func createSticker(_ localizedDescription: String) throws -> MSSticker? {
+        // See: https://developer.apple.com/documentation/messages
+        // Ordered from largest to smallest.
+        let optimalStickerDimensionsPx: [CGFloat] = [ 618.0, 408.0, 300.0 ]
+        let maxStickerFileSizeBytes: Int32 = 500 * 1024
+
         // Find optimal sticker size.
         var stickerDimensions = self.size
-        let candidateStickerDimensions = UIImage.OPTIMAL_STICKER_DIMENSIONS_PX.filter { $0 <= self.size.width && $0 <= self.size.height }
+        let candidateStickerDimensions = optimalStickerDimensionsPx.filter {
+            $0 <= self.size.width && $0 <= self.size.height
+        }
         if !candidateStickerDimensions.isEmpty {
             let stickerDimension = candidateStickerDimensions[0]
             stickerDimensions = CGSize(width: stickerDimension, height: stickerDimension)
@@ -88,8 +95,8 @@ extension UIImage {
             return nil
         }
         let saveSuccss = haveAlphaChannel
-            ? UIImage.saveImageAsPng(sourceImage, filePath: filePath, maxSize: UIImage.MAX_STICKER_FILE_SIZE_BYTES)
-            : UIImage.saveImageAsJpeg(sourceImage, filePath: filePath, maxSize: UIImage.MAX_STICKER_FILE_SIZE_BYTES)
+            ? sourceImage.saveImageAsPng(filePath, maxSize: maxStickerFileSizeBytes)
+            : sourceImage.saveImageAsJpeg(filePath, maxSize: maxStickerFileSizeBytes)
         if !saveSuccss {
             // Failed to save sticker source image to file.
             return nil
@@ -100,22 +107,22 @@ extension UIImage {
             localizedDescription: localizedDescription)
     }
 
-    static func saveImageAsPng(_ image: UIImage, filePath: String, maxSize: Int32 = Int32.max) -> Bool {
-        guard let cgImage = image.cgImage, let copyOfCgImage = cgImage.copy() else {
+    func saveImageAsPng(_ filePath: String, maxSize: Int32 = Int32.max) -> Bool {
+        guard let cgImage = self.cgImage, let copyOfCgImage = cgImage.copy() else {
             // Failed to get copy of underlying CGImage.
             return false
         }
         // We attempt to save the file to meet the maximum size constraint by adjusting image dimensions.
-        let initialSize = image.size
+        let initialSize = self.size
         let sizeIncrementFactor: CGFloat = 0.10
         var currentSize = initialSize
         var currentImage = UIImage(cgImage: copyOfCgImage)
         while currentSize.width * currentSize.height > 0.0 {
             do {
-                try UIImagePNGRepresentation(currentImage)?.write(to: URL(fileURLWithPath: filePath), options: .atomic)
+                try UIImagePNGRepresentation(currentImage)?.write(to: URL(fileURLWithPath: filePath),
+                                                                  options: .atomic)
                 let attrs = try FileManager.default.attributesOfItem(atPath: filePath)
                 if let fileSize = attrs[FileAttributeKey.size] as? NSNumber {
-                    print("lossless file size is \(fileSize)")
                     if fileSize.uint64Value > maxSize {
                         currentSize = CGSize(
                             width: currentSize.width - (currentSize.width * sizeIncrementFactor),
@@ -141,14 +148,15 @@ extension UIImage {
         return false
     }
 
-    static func saveImageAsJpeg(_ image: UIImage, filePath: String, maxSize: Int32 = Int32.max) -> Bool {
+    func saveImageAsJpeg(_ filePath: String, maxSize: Int32 = Int32.max) -> Bool {
         // We attempt to save the file to meet the maximum size constraint by adjusting compression quality.
         let initialQuality: CGFloat = 0.9
         let qualityIncrement: CGFloat = 0.05
         var currentQuality: CGFloat = initialQuality
         while currentQuality > 0.0 {
             do {
-                try UIImageJPEGRepresentation(image, currentQuality)?.write(to: URL(fileURLWithPath: filePath), options: .atomic)
+                try UIImageJPEGRepresentation(self, currentQuality)?.write(to: URL(fileURLWithPath: filePath),
+                                                                           options: .atomic)
                 let attrs = try FileManager.default.attributesOfItem(atPath: filePath)
                 if let fileSize = attrs[FileAttributeKey.size] as? NSNumber {
                     if fileSize.uint64Value > maxSize {
@@ -168,5 +176,5 @@ extension UIImage {
         }
         return false
     }
-    
+
 }
